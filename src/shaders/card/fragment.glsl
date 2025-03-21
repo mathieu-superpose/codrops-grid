14a98b87a204f8fe278e9cf6ca9a002e73eddeee
Mathieu Joly <superpose@minimac.local>
2025-03-21 21:00:15 +0100

    add altered colors

--- src/shaders/card/fragment.glsl
+++ src/shaders/card/fragment.glsl
@@ -4,8 +4,15 @@ uniform float uDistance;
 varying vec2 vUv;
 
 vec3 getLuminance(vec3 color) {
-    vec3 luminance = vec3(0.2126, 0.7152, 0.0722);
+    // vec3 luminance = vec3(0.2126, 0.7152, 0.0722);
+    vec3 luminance = vec3(0.2, 0.75, 0.1);
     return vec3(dot(luminance, color));
+}
+
+vec3 alteredColors(vec3 inputColor) {
+    const mat3 Mat = mat3(0.5, 0.5, 0.1, 0.5, 0.1, 0.1, 0.1, 0.5, 0.1);
+
+    return inputColor * Mat;
 }
 
 void main() {
@@ -13,7 +20,10 @@ void main() {
     float distanceFactor = min(max(uDistance, 0.), 1.);
 
     vec3 imageLum = getLuminance(image.xyz);
-    vec3 color = mix(image.xyz, imageLum, distanceFactor);
 
-    gl_FragColor = vec4(color, 1.);
+    vec3 texColor = alteredColors(image.xyz);
+
+    vec3 color = mix(texColor, imageLum, distanceFactor);
+
+    gl_FragColor = vec4(color, 1.0);
 }
